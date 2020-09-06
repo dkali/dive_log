4b2a757d2a60b11c6e313d5ce7f0ddc7a4e2b2c1
dkali <secretshelter@gmail.com>
2020-09-06 22:50:08 +0300

    email/pswd login added

--- src/components/SignInScreen.js
+++ src/components/SignInScreen.js
@@ -2,10 +2,67 @@
 import React from 'react';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import firebase from 'firebase';
-import App from './App'
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 import { Redirect } from 'react-router';
 
+const centered_style = {
+  position: "absolute",
+  left: "40%",
+}
+
+var sign_in_states = Object.freeze({"unknown": 0, "sign_in_required": 1, "logged_in": 2})
+
 class SignInScreen extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isSignedIn: sign_in_states.unknown,
+    }
+
+    this.handleSignInClick = this.handleSignInClick.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePswdChange = this.handlePswdChange.bind(this);
+  }
+
+  handleEmailChange(event) {
+    this.setState({email: event.target.value});
+  }
+
+  handlePswdChange(event) {
+    this.setState({password: event.target.value});
+  }
+
+  // Listen to the Firebase Auth state and set the local state.
+  componentDidMount() {
+    this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(
+      (user) => {
+        if (user) {
+          this.setState({isSignedIn: sign_in_states.logged_in});
+        } else {
+          this.setState({isSignedIn: sign_in_states.sign_in_required});
+        }
+      }
+    );
+  }
+
+  // Make sure we un-register Firebase observers when the component unmounts.
+  componentWillUnmount() {
+    this.unregisterAuthObserver();
+  }
+
+  handleSignInClick(){
+    firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+    .then(function(firebaseUser) {
+      // Success
+    })
+    .catch(function(error) {
+      // Handle Errors here. TODO
+      var errorCode = error.code;
+      var errorMessage = error.message;
+    });
+  }
   // Configure FirebaseUI.
   uiConfig = {
     // Popup signin flow rather than redirect flow.
@@ -16,7 +73,7 @@ class SignInScreen extends React.Component {
       firebase.auth.FacebookAuthProvider.PROVIDER_ID
     ],
     // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-    signInSuccessUrl: '/',
+    // signInSuccessUrl: '/',
     // callbacks: {
     //   // Avoid redirects after sign-in.
     //   signInSuccessWithAuthResult: () => false
@@ -25,11 +82,28 @@ class SignInScreen extends React.Component {
 
   render() {
     console.log("sign-in: render login page");
+
+    if (this.state.isSignedIn === sign_in_states.logged_in) {
+      return <Redirect push to="/" />;
+    }
+
     return (
       <div>
         <h1>My App</h1>
         <p>Please sign-in:</p>
         <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()}/>
+
+        <div style={centered_style}>
+          <div>
+            <TextField id="standard-basic" label="email" onChange={this.handleEmailChange}/>
+          </div>
+          <div>
+            <TextField id="standard-basic" label="password" onChange={this.handlePswdChange}/>
+          </div>
+          <div>
+            <Button variant="contained" color="primary" onClick={this.handleSignInClick}>Sign-in</Button>
+          </div>
+        </div>
       </div>
     );
   }

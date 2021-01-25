d2835d190c506e15dd8255eea8c9c504095ce25c
dkali <secretshelter@gmail.com>
2021-01-25 20:45:24 +0300

    Map rewritten to function

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -11,62 +11,60 @@ const containerStyle = {
   height: '100%',
 }
 
-export class MapContainer extends React.Component {
-  createMarker(location) {
+function MapContainer(props) {
+  const createMarker = (location) => {
     return (<Marker
       title={location.name}
       name={location.name}
       key={location.geopoint.latitude + location.geopoint.longitude}
       position={{ lat: location.geopoint.latitude, lng: location.geopoint.longitude }}
-      onClick={this.props.onMarkerClick}
+      onClick={props.onMarkerClick}
       // custom fields
       loc_id={location.loc_id}
       geopoint={location.geopoint}
     />)
   }
 
-  render() {
-    const { dives, cur_dive } = this.props;
+  const { dives, cur_dive } = props;
 
-    // filter for only unique locations
-    let uniq_locations = new Set();
-    let listItems = [];
-    dives.forEach(dive => {
-      if (uniq_locations.has(dive.location.name) === false) {
-        uniq_locations.add(dive.location.name);
-        listItems.push(this.createMarker(dive.location));
-      }
-    });
-
-    // center map on selected dive, or a last known dive, if nothing selected
-    var center_lat = 56.336893;
-    var center_lon = 43.986196;
-    if (dives.length > 0) {
-      center_lat = cur_dive === undefined ? dives[dives.length - 1].location.geopoint.latitude : cur_dive.location.geopoint.latitude;
-      center_lon = cur_dive === undefined ? dives[dives.length - 1].location.geopoint.longitude : cur_dive.location.geopoint.longitude;
+  // filter for only unique locations
+  let uniq_locations = new Set();
+  let listItems = [];
+  dives.forEach(dive => {
+    if (uniq_locations.has(dive.location.name) === false) {
+      uniq_locations.add(dive.location.name);
+      listItems.push(createMarker(dive.location));
     }
+  });
 
-    return (
-      <Map google={this.props.google}
-        zoom={9}
-        style={this.props.map_style}
-        initialCenter={{
-          lat: center_lat,
-          lng: center_lon
-        }}
-        center={{
-          lat: center_lat,
-          lng: center_lon
-        }}
-        containerStyle={containerStyle}
-        onClick={this.props.mapClicked}
-      >
-
-        {listItems}
-        {this.props.newMarker != null && this.props.newMarker}
-      </Map>
-    );
+  // center map on selected dive, or a last known dive, if nothing selected
+  var center_lat = 56.336893;
+  var center_lon = 43.986196;
+  if (dives.length > 0) {
+    center_lat = cur_dive === undefined ? dives[dives.length - 1].location.geopoint.latitude : cur_dive.location.geopoint.latitude;
+    center_lon = cur_dive === undefined ? dives[dives.length - 1].location.geopoint.longitude : cur_dive.location.geopoint.longitude;
   }
+
+  return (
+    <Map google={props.google}
+      zoom={9}
+      style={props.map_style}
+      initialCenter={{
+        lat: center_lat,
+        lng: center_lon
+      }}
+      center={{
+        lat: center_lat,
+        lng: center_lon
+      }}
+      containerStyle={containerStyle}
+      onClick={props.mapClicked}
+    >
+
+      {listItems}
+      {props.newMarker != null && props.newMarker}
+    </Map>
+  );
 }
 
 const mapStateToProps = state => {

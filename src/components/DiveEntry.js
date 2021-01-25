7737daf5130b580b8a40ea46421be22873e03e44
dkali <secretshelter@gmail.com>
2021-01-25 12:19:52 +0300

    DiveEntry rewritten to function

--- src/components/DiveEntry.js
+++ src/components/DiveEntry.js
@@ -24,81 +24,73 @@ const styles = theme => ({
   }
 });
 
-class DiveEntry extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    this.props.selectDive(this.props.firestore_id);
-    if (this.props.handleTabChange !== undefined) {
-      this.props.handleTabChange(null, 1);
+function DiveEntry(props) {
+  const handleClick = () => {
+    props.selectDive(props.firestore_id);
+    if (props.handleTabChange !== undefined) {
+      props.handleTabChange(null, 1);
     }
   }
 
-  render() {
-    const { entryData, dive_num } = this.props;
-    const { classes } = this.props;
+  const { entryData, dive_num, classes } = props;
 
-    return (
-      <ListItem className={classes.root} onClick={this.handleClick}>
-        <Paper className={classes.paper}>
-          <Box bgcolor="primary.main" color="primary.contrastText" p={2}>
-            <Grid container>
-              <Grid item xs container direction="column" spacing={2}>
-                <Typography variant="subtitle1" gutterBottom>
-                  {entryData.location.name}
-                </Typography>
-                <Grid
-                  container
-                  direction="row"
-                  justify="flex-start"
-                  alignItems="flex-start"
-                >
-                  <Grid item xs={2} container direction="row" alignItems="center">
-                    <Box p={1}>
-                      <Check fontSize="small" />
-                    </Box>
-                    <Typography variant="body2" gutterBottom>
-                      {dive_num}
-                    </Typography>
-                  </Grid>
-
-                  <Grid item xs={2} container direction="row" alignItems="center">
-                    <Box p={1}>
-                      <Height fontSize="small" />
-                    </Box>
-                    <Typography variant="body2" gutterBottom>
-                      {entryData.depth}
-                    </Typography>
-                  </Grid>
-
-                  <Grid item xs={2} container direction="row" alignItems="center">
-                    <Box p={1}>
-                      <Timelapse fontSize="small" />
-                    </Box>
-                    <Typography variant="body2" gutterBottom>
-                      {entryData.duration}
-                    </Typography>
-                  </Grid>
-
+  return (
+    <ListItem className={classes.root} onClick={handleClick}>
+      <Paper className={classes.paper}>
+        <Box bgcolor="primary.main" color="primary.contrastText" p={2}>
+          <Grid container>
+            <Grid item xs container direction="column" spacing={2}>
+              <Typography variant="subtitle1" gutterBottom>
+                {entryData.location.name}
+              </Typography>
+              <Grid
+                container
+                direction="row"
+                justify="flex-start"
+                alignItems="flex-start"
+              >
+                <Grid item xs={2} container direction="row" alignItems="center">
+                  <Box p={1}>
+                    <Check fontSize="small" />
+                  </Box>
+                  <Typography variant="body2" gutterBottom>
+                    {dive_num}
+                  </Typography>
                 </Grid>
-              </Grid>
-              <Grid item>
-                <Typography variant="subtitle1">{format(fromUnixTime(entryData.date.seconds), "MMM dd, yyyy")}</Typography>
-                <IconButton>
-                  <NavLink to={`/dive/${this.props.firestore_id}`}>
-                    <Edit />
-                  </NavLink >
-                </IconButton>
+
+                <Grid item xs={2} container direction="row" alignItems="center">
+                  <Box p={1}>
+                    <Height fontSize="small" />
+                  </Box>
+                  <Typography variant="body2" gutterBottom>
+                    {entryData.depth}
+                  </Typography>
+                </Grid>
+
+                <Grid item xs={2} container direction="row" alignItems="center">
+                  <Box p={1}>
+                    <Timelapse fontSize="small" />
+                  </Box>
+                  <Typography variant="body2" gutterBottom>
+                    {entryData.duration}
+                  </Typography>
+                </Grid>
+
               </Grid>
             </Grid>
-          </Box>
-        </Paper>
-      </ListItem>
-    )
-  }
+            <Grid item>
+              <Typography variant="subtitle1">{format(fromUnixTime(entryData.date.seconds), "MMM dd, yyyy")}</Typography>
+              <IconButton>
+                <NavLink to={`/dive/${props.firestore_id}`}>
+                  <Edit />
+                </NavLink >
+              </IconButton>
+            </Grid>
+          </Grid>
+        </Box>
+      </Paper>
+    </ListItem>
+  )
 }
 
 export default connect(
